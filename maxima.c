5daf567d8e24f36c90143a0f998580a0793d9082
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-02-21 16:32:41 +0530

    maxima.c
    maximum

---
+++ maxima.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[100],b,n=5,i,j;
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+b=a[i];
+a[i]=a[j];
+a[j]=b;
+}
+}
+}
+printf("\n%d",a[n]);
+return 0;
+}
